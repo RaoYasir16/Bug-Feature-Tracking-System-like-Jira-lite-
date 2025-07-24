8cb6154381996b712878b8dc0c49888dd8646500
unknown <raoyasirmajeed140@gmail.com>
2025-07-24 14:39:29 +0500

    Project CRUD

--- controllers/project/index.js
+++ controllers/project/index.js
@@ -1,9 +1,12 @@
-const {create, getAll} = require('../../services/project/index');
+const {create, getAll, getById, updateProject, remove} = require('../../services/project/index');
 const router = require('express').Router();
 
 
 router.post('/add-project',create);
-router.get('/all-projects',getAll)
+router.get('/all-projects',getAll);
+router.get('/get-project/:id',getById);
+router.put('/update-project/:id',updateProject);
+router.delete('/delete-project/:id',remove)
 
 
 module.exports = router
--- services/project/index.js
+++ services/project/index.js
@@ -25,6 +25,53 @@ const getAll = asyncErrorHandler(async(req,res)=>{
     })
 })
 
+const getById = asyncErrorHandler(async(req,res)=>{
+    const id = req.params.id
+    const project = await Project.findOne({where:{id}});
+    if(!project){
+        return res.status(STATUS_CODE.NOT_FOUND).json({
+            message:TEXTS.NOT_FOUND
+        })
+    }
+
+    return res.status(STATUS_CODE.SUCCESS).json({
+        project
+    })
+});
+
+const updateProject = asyncErrorHandler(async(req,res)=>{
+    const project = await Project.findByPk(req.params.id);
+    if(!project){
+        return res.status(STATUS_CODE.NOT_FOUND).json({
+            message:TEXTS.NOT_FOUND
+        })
+    }
+
+    await project.update(req.body);
+
+    const updatedProject = await project.save()
 
 
-module.exports = {create,getAll}
+    return res.status(STATUS_CODE.SUCCESS).json({
+        message:"Project updated successfylly",
+        updatedProject
+    })
+});
+
+const remove= asyncErrorHandler(async(req,res)=>{
+    const project = await Project.findByPk(req.params.id);
+
+    if(!project){
+        return res.status(STATUS_CODE.NOT_FOUND).json({
+            message:TEXTS.NOT_FOUND
+        })
+    }
+
+    await project.destroy();
+
+    return res.status(STATUS_CODE.SUCCESS).json({
+        message:"Project Delete Successfylly"
+    })
+})
+
+module.exports = {create,getAll,getById,updateProject,remove}
